feb257c0492efacbe3d903cf38eb3532d99b1ee7
udw4817 <udw4817@gmail.com>
2016-09-28 15:20:15 +0100

    restructuring folders and files

---
+++ toolboxIIP-project/javascript.js
@@ -0,0 +1,7 @@
+$(document).ready(function(){
+    //Handles menu drop down
+    $('.dropdown-menu').find('form').click(function (e) {
+        e.stopPropagation();
+    });
+});
+$<button onclick="project1.index.html">Get Started</button>
